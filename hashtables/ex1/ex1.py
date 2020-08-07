9a26095de28954a222e313cc7218982e669efee4
M Groesbeck <m.groesbeck@gmail.com>
2020-08-07 08:37:28 -0700

    Passing tests for ex1.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,20 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    if length < 2:
+        return None
+    wt_tbl = {}
+    for i in range(len(weights)):
+        if weights[i] in wt_tbl:
+            wt_tbl[weights[i]].append(i)
+        else:
+            wt_tbl[weights[i]] = [i]
 
+    for wt in weights:
+        if (limit - wt) in wt_tbl:
+            print(f"Found: {wt}, {limit-wt}")
+            for i in wt_tbl[wt]:
+                for j in wt_tbl[limit-wt]:
+                    if i != j:
+                        return (max(i,j), min(i,j))
     return None
